87a1586e0ca67f09c6c4dbe1696c277a185b3546
Bharti <bhartipandit098@gmail.com>
2026-01-28 23:31:22 +0530

    react-agent working

--- main.py
+++ main.py
@@ -1,35 +1,95 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description, tool, Tool
 from langchain_openai import ChatOpenAI
-import os
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_classic.agents.output_parsers import ReActSingleInputOutputParser
+from typing import List, Union, Tuple
+from callbacks import AgentCallbackHandler
+
 load_dotenv()
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_openai import chat_models
-from langchain_tavily import TavilySearch
-from typing import List
-from pydantic import BaseModel, Field
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-    url:str=Field(description="the URL of the source")
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
 
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-    answer:str = Field(description="The agent's answer to the query")
-    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
-
-llm = ChatOpenAI(model="gpt-5")
-tools = [TavilySearch(
-    max_results=5
-)]
-agent = create_agent(model=llm,tools=tools,response_format=AgentResponse)
+    return len(text)
 
 def main():
-    print("Hello from langchain")
-    result = agent.invoke({"messages":HumanMessage(content="Give me list of job openings posted within 24 hours on Linkedin for agentic ai")})
-    print(result)
+    print("Hello from ReAct LangChain!")
+    print(get_text_length.func("Dog"))
+
+def find_tool_by_name(tools:List[Tool], tool_name:str):
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+        raise ValueError(f"Tool with name {tool_name} not found")
+    
+def format_log_to_str(
+        intermediate_steps: List[Tuple[AgentAction,str]],
+        observation_prefix: str="Observation: ",
+        llm_prefix: str = "Thought"
+) -> str:
+    """Construct the scratchpad that lets agent continue its thought process"""
+    thoughts = ""
+    for action, observation in intermediate_steps:
+        thoughts += action.log
+        thoughts += f"\n{observation_prefix}-{observation}\n{llm_prefix}"
+    return thoughts
 
 if __name__ == "__main__":
     main()
+    print("Hello ReAct LangChain!")
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"], callbacks=[AgentCallbackHandler()])
+    intermediate_steps = []
+    agent = {"input":lambda x:x["input"], "agent_scratchpad": lambda x:format_log_to_str(x["agent_scratchpad"])} | prompt | llm | ReActSingleInputOutputParser()
+
+    agent_step = ""
+    while not isinstance(agent_step,AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input":"What is the text length of 'DOG' in characters?","agent_scratchpad":intermediate_steps})
+
+        print(agent_step)
+
+        if isinstance(agent_step,AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools,tool_name)
+            tool_input = agent_step.tool_input
+            observation = tool_to_use.func(str(tool_input))
+            print(observation)
+            intermediate_steps.append((agent_step, str(observation)))
+
+    if isinstance(agent_step, AgentAction):
+        print(agent_step.return_values)
