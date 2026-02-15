a1254257b2c8dd67ff082c91962a5244cc7e527a
Bharti <bhartipandit098@gmail.com>
2026-02-15 08:07:57 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from langgraph.graph import END, MessagesState, StateGraph
+from nodes import run_agent_reasoning, tool_node
+
+load_dotenv()
+
+AGENT_REASON="agent_reason"
+ACT="act"
+LAST=-1
+
+flow = StateGraph(MessagesState)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+
+def should_continue(state: MessagesState):
+    """Route to tool node if the last message has tool calls, otherwise END."""
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {END: END, ACT: ACT})
+flow.add_edge(ACT, AGENT_REASON)
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+if __name__=="__main__":
+    print("Hello react langgraph with function calling")
+    
